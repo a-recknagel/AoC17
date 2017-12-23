160da7442f05bb255d1034722bdf63f700e85be5
a-recknagel <arne.recknagel@hotmail.com>
2017-12-23 23:40:30 +0100

    bad code
    why am i even uploading this

--- advent/advent_17/advent.py
+++ advent/advent_17/advent.py
@@ -1,20 +1,49 @@
-from datetime import datetime
 
+def advent_17():
 
-def day_17():
-    puzzle_loc = '.txt'
+    class Link:
+        length = 0
+        root = None
 
-    def puzzle_1(input_str):
-        pass
-    yield puzzle_1(puzzle_loc)
+        def __init__(self, val, before_link=None):
+            if before_link is None:
+                before_link = self
+                self.next_link = self
+                Link.root = self
+            self.val = val
+            self.next_link = before_link.next_link
+            before_link.next_link = self
+            Link.length += 1
 
-    def puzzle_2(input_str):
-        pass
-    yield puzzle_2(puzzle_loc)
+        def __str__(self):
+            current = Link.root
+            ret = '['
+            for _ in range(Link.length):
+                if current == self:
+                    ret += '(' + str(current.val) + '), '
+                else:
+                    ret += str(current.val) + ', '
+                current = current.next_link
+            return ret[:-2] + ']'
 
-now = datetime.now()
-for solution_17 in day_17():
-    print(solution_17)
-    pass
-print("Took a total of %.3f seconds\n" % (datetime.now() - now).total_seconds())
+    def puzzle_1(step_nr):
+        current = Link(0)
+        for counter in range(1, 2018):
+            for _ in range(step_nr % Link.length):
+                current = current.next_link
+            current = Link(counter, current)
+        return current.next_link.val
+    yield puzzle_1(363)
 
+    def puzzle_2(step_nr):
+        current = 0
+        ret = 1
+        for idx in range(1, 50000001):
+            current = ((current + step_nr) % idx) + 1
+            if current == 1:
+                ret = idx
+        return ret
+    yield puzzle_2(363)
+
+for answer in advent_17():
+    print(answer)
